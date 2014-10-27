4306e1f7840208ecb3f11a36922fd98784a4fed2
HERNANDEZ HERNANDEZ <alu4531@cc112.(none)>
2014-10-27 15:08:03 +0000

    Implementando la clase Listaç

--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -8,36 +8,37 @@ describe Exam do
 	end
 
 	describe 'Seleccion simple' do
-		it 'Debe existir una pregunta' do
+		it 'Debe existir una pregunta.' do
 			expect(@p1.pr).to eq("¿Pregunta?")
 		end
 		
-		it 'Debe existir un metodo para obtener la pregunta' do
+		it 'Debe existir un metodo para obtener la pregunta.' do
 			expect(@p1.preg).to eq("Pregunta: ¿Pregunta?")
 		end
 
-		it 'Deben existir opciones de respuesta(introducir respuesta)' do
+		it 'Deben existir opciones de respuesta(introducir respuesta).' do
 			@p1.addr("Si.")
 			expect(@p1.re).not_to be_empty
 		end
 	
-		it 'Se debe invocar a un metodo para obtener las opciones de respuesta' do
+		it 'Se debe invocar a un metodo para obtener las opciones de respuesta.' do
 			@p1.addr("No.")
 			expect(@p1.resp).to eq("1) Si.\n2) No.\n")
 		end
 	
-		it 'Se debe mostrar por consola la pregunta y las opciones de respuesta' do
+		it 'Se debe mostrar por consola la pregunta y las opciones de respuesta.' do
 			expect(@p1.to_s).to eq("Pregunta: ¿Pregunta?\n\n1) Si.\n2) No.\n")
 		end
 	end
 
 	describe 'Lista enlazada' do
 		before :all do
-			@lista = Lista
-		end	
+			@lista = Lista.new()
+		end
+		
+		it 'Debe ser posible insertar nodos en la lista.'
+			@nodo = Nodo.new("Primer nodo.")
+			@lista.addn(@nodo)
+			expect(@lista.head.to_s).to eq("Primer nodo.")
+		end
 	end
-
-
-end
-
-
